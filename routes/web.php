1dc6072efab7932abbf6d00ece0aa120369142cc
FredySaputra <lilincemerlang@gmail.com>
2026-02-19 14:27:59 +0700

    Add create todo form page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('todos.index');
 });
+
+Route::get('/todos/create', function () {
+    return view('todos.create');
+});
+
+Route::post('/todos', function () {
+    return redirect('/todos')->with('success', 'Todo berhasil ditambahkan!');
+});
+
